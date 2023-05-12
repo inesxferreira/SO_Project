e3baeca07d834d92878b5ef764f05dae5d542249
anaritaasp <ritaa3008@gmail.com>
2023-05-12 01:03:38 +0100

    commented some stuff

--- structs.h
+++ structs.h
@@ -2,18 +2,18 @@
 
 #define CLIENT_TO_SERVER "tmp/clientToserver"
 
-//struct que define um no
+//struct que define um novo pedido do cliente
 typedef struct pedido
 {
     int pid;
-    struct timeval  initial_timestamp; // tempo inicial em segundos
+    struct timeval  initial_timestamp; 
 }PEDIDO;
 
 // struct que guarda os pedidos em execução num dado momento
 typedef struct pedidos_execucao{
     char nome_programa[512];
     int pid;
-    long initial_timestamp; // tempo inicial em segundos
+    long initial_timestamp; 
 } PEDIDOSEXECUCAO;
 
 
